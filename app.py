469413173e29feb6ee9f06da9c520923a08cc83a
unknown <pyptom76@gmail.com>
2022-04-25 20:28:00 +0800

    bug fixed

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ app.secret_key = 'jose'
 api = Api(app)
 
 
-@app.before_request
-def create_tables():
-    db.create_all()
+# @app.before_request
+# def create_tables():
+#     db.create_all()
 
 
 jwt = JWT(app, authenticate, identity)  # /auth
